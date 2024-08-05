6b34b20b3149161242729d892fa878c16419b2d2
redajaifar <reda.jaifar@thalesdigital.io>
2024-08-05 17:19:00 +0200

    feat: redesign contact page

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -7,6 +7,7 @@ import { toast } from "sonner";
 import { translate } from "@docusaurus/Translate";
 import Layout from "@theme/Layout";
 import ContentLayout from "@site/src/components/ContentLayout";
+import styles from "./contact.module.css";
 
 function Contact() {
   const {
@@ -51,66 +52,55 @@ function Contact() {
   return (
     <Layout>
       <ContentLayout>
-        <section>
+        <section className={styles.rootContainer}>
           <Toaster dir="auto" position="top-center" duration={9000} />
-          <div className="mx-auto max-w-screen-md px-4 py-8 lg:py-16">
-            <h2 className="mb-4 text-center text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white">
-              Contact Us
-            </h2>
-            <p className="mb-8 text-center font-light text-gray-500 dark:text-gray-400 sm:text-xl lg:mb-16">
+          <div className={styles.titleContainer}>
+            <h2 className={styles.title}>Contact Us</h2>
+            <p className={styles.subTitle}>
               Let&apos;s get the conversation started, at your service
             </p>
-            <form action="" onSubmit={sendMessage} className="space-y-8">
+            <form action="" onSubmit={sendMessage} className={styles.form}>
               <div>
-                <label
-                  htmlFor="email"
-                  className="mb-2 block text-sm font-medium text-gray-900 dark:text-gray-300"
-                >
+                <label htmlFor="email" className={styles.formLabel}>
                   Your email
                 </label>
                 <input
                   ref={emailRef}
                   type="email"
                   id="email"
-                  className="dark:shadow-sm-light block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500"
+                  className={styles.emailInput}
                   placeholder="your email"
                   required
                 />
               </div>
               <div>
-                <label
-                  htmlFor="subject"
-                  className="mb-2 block text-sm font-medium text-gray-900 dark:text-gray-300"
-                >
+                <label htmlFor="subject" className={styles.formLabel}>
                   Subject
                 </label>
                 <input
                   ref={subjectRef}
                   type="text"
                   id="subject"
-                  className="dark:shadow-sm-light block w-full rounded-lg border border-gray-300 bg-gray-50 p-3 text-sm text-gray-900 shadow-sm focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500"
+                  className={styles.subjectInput}
                   placeholder="Let us know how we can help you"
                   required
                 />
               </div>
               <div className="sm:col-span-2">
-                <label
-                  htmlFor="message"
-                  className="mb-2 block text-sm font-medium text-gray-900 dark:text-gray-400"
-                >
+                <label htmlFor="message" className={styles.formLabel}>
                   Your message
                 </label>
                 <textarea
                   ref={messageRef}
                   id="message"
                   rows={6}
-                  className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-primary-50 dark:focus:ring-primary-500"
+                  className={styles.messageInput}
                   placeholder="Leave a comment..."
                 ></textarea>
               </div>
               <button
                 type="submit"
-                className="rounded-lg bg-primary-50 px-5 py-3 text-center text-sm font-medium hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 sm:w-fit"
+                className={styles.submitButton}
               >
                 Send message
               </button>
