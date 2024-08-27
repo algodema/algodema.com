4e6fa23a9592226d806962fc6932934978b129e1
redajaifar <reda.jaifar@thalesdigital.io>
2024-08-27 16:47:27 +0200

    Deploy website - based on c4952e3c3946800fa965156e6c288097e40db648

---
+++ assets/js/acecf23e.0ebbffe8.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/algodema.com/blog","blogTitle":"Blog","authorsListPath":"/algodema.com/blog/authors"}')}}]);
