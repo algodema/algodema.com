c2a993627e8bc868fbea8983d6551eb0a0f60fd0
redajaifar <reda.jaifar@thalesdigital.io>
2024-08-27 16:48:18 +0200

    build: build and deploy

---
+++ build/assets/js/bc048dc6.61ba6f77.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[133],{7634:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/algodema.com/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":5,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
